08bb6f4bef39083908a37954e177ac3e1afb7818
shadvoniicc <moxeyymarcu28@gmail.com>
2023-10-27 11:03:40 -0400

    ediit program.cs

--- Quiz XML Generator 0.1.3 asof 2023-10-27-0937/QuizApiApp/QuizApi/Program.cs
+++ Quiz XML Generator 0.1.3 asof 2023-10-27-0937/QuizApiApp/QuizApi/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -18,8 +29,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// Use CORS before other middleware and after HTTPS redirection
+app.UseCors();
 
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
